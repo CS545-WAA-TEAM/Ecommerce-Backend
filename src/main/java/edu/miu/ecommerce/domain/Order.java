1537d757b65a8e86c6c006e30af808e56ca1e705
beeAk <se.blen.aklilu@gmail.com>
2021-05-10 18:34:09 -0500

    add order model

---
+++ src/main/java/edu/miu/ecommerce/domain/Order.java
@@ -0,0 +1,39 @@
+package edu.miu.ecommerce.domain;
+
+import edu.miu.ecommerce.utils.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name="buyer_id")
+    private Buyer buyer;
+
+    @ManyToOne
+    @JoinColumn(name="seller_id")
+    private Seller seller;
+
+    @OneToOne
+    private Address shippingAddress;
+
+    @OneToOne
+    private Address billingAddress;
+
+
+}
---
+++ src/main/java/edu/miu/ecommerce/utils/OrderStatus.java
@@ -0,0 +1,6 @@
+package edu.miu.ecommerce.utils;
+
+public enum OrderStatus {
+
+    SHIPPED, ON_THE_WAY, CANCELLED, DELIVERED
+}
