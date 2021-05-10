7144265af30579c437da7d29b51a769337af191a
beeAk <se.blen.aklilu@gmail.com>
2021-05-10 18:30:26 -0500

    add address model

---
+++ src/main/java/edu/miu/ecommerce/domain/Address.java
@@ -0,0 +1,28 @@
+package edu.miu.ecommerce.domain;
+
+import edu.miu.ecommerce.utils.AddressType;
+
+import javax.persistence.*;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zip_code")
+    private int zipCode;
+
+    @Column(name = "country")
+    private String country;
+
+    private AddressType addressType;
+
+}
---
+++ src/main/java/edu/miu/ecommerce/utils/AddressType.java
@@ -0,0 +1,6 @@
+package edu.miu.ecommerce.utils;
+
+public enum AddressType {
+
+    BILLING, SHIPPING
+}
