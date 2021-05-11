abc1584b9d7105d9fb349ba59426c264e65d1329
JaCkIsO <jackisodev@gmail.com>
2021-05-10 22:47:33 -0500

    Create Audit.java

---
+++ src/main/java/edu/miu/ecommerce/domain/Audit.java
@@ -0,0 +1,22 @@
+package edu.miu.ecommerce.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Audit {
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreatedDate
+    private long createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private long modifiedDate;
+}
