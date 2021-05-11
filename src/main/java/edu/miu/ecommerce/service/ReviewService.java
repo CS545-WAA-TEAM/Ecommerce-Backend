2d8a858c54a2500e59f92ec8f7a8588f936fdb24
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:53:22 -0500

    review service

--- src/main/java/edu/miu/ecommerce/service/ReviewService.java
+++ src/main/java/edu/miu/ecommerce/service/ReviewService.java
@@ -1,7 +1,17 @@
 package edu.miu.ecommerce.service;
 
+import edu.miu.ecommerce.domain.Product;
+import edu.miu.ecommerce.domain.Review;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ReviewService {
+
+    Iterable<Review> getAllReviews();
+
+    Review getReviewById(long id);
+
+    Review addReview(Review review);
+
+    void deleteReview(long id);
 }
--- src/main/java/edu/miu/ecommerce/service/implementation/ReviewServiceImpl.java
+++ src/main/java/edu/miu/ecommerce/service/implementation/ReviewServiceImpl.java
@@ -1,6 +1,26 @@
 package edu.miu.ecommerce.service.implementation;
 
+import edu.miu.ecommerce.domain.Review;
 import edu.miu.ecommerce.service.ReviewService;
 
 public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public Iterable<Review> getAllReviews() {
+        return null;
+    }
+
+    @Override
+    public Review getReviewById(long id) {
+        return null;
+    }
+
+    @Override
+    public Review addReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public void deleteReview(long id) {
+
+    }
 }
