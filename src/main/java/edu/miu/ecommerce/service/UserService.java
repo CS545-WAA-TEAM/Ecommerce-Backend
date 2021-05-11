aeef4623b42ee0ae377797b3fe8b8c2af6165854
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:54:08 -0500

    user service

--- src/main/java/edu/miu/ecommerce/service/UserService.java
+++ src/main/java/edu/miu/ecommerce/service/UserService.java
@@ -1,7 +1,17 @@
 package edu.miu.ecommerce.service;
 
+import edu.miu.ecommerce.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
+
+    Iterable<User> getAllUsers();
+
+    User getUserById(long id);
+
+    User addUser(User user);
+
+    void deleteUser(long id);
+
 }
