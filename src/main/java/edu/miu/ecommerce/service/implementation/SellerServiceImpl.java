f80e939fbc98b381501a1539b4fac7712923324e
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:53:39 -0500

    Seller service

--- src/main/java/edu/miu/ecommerce/service/implementation/SellerServiceImpl.java
+++ src/main/java/edu/miu/ecommerce/service/implementation/SellerServiceImpl.java
@@ -1,6 +1,34 @@
 package edu.miu.ecommerce.service.implementation;
 
+import edu.miu.ecommerce.domain.Product;
+import edu.miu.ecommerce.domain.Seller;
 import edu.miu.ecommerce.service.SellerService;
 
+import java.util.List;
+
 public class SellerServiceImpl implements SellerService {
+    @Override
+    public Iterable<Seller> getAllSellers() {
+        return null;
+    }
+
+    @Override
+    public Seller getSellerById(long id) {
+        return null;
+    }
+
+    @Override
+    public Seller addSeller(Seller seller) {
+        return null;
+    }
+
+    @Override
+    public void deleteSeller(long id) {
+
+    }
+
+    @Override
+    public List<Product> findProducts(long id) {
+        return null;
+    }
 }
