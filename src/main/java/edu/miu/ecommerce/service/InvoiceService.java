d17fc4e771f91b6956349e468974fa7965e8a2fb
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:52:37 -0500

    Invoice service

--- src/main/java/edu/miu/ecommerce/service/InvoiceService.java
+++ src/main/java/edu/miu/ecommerce/service/InvoiceService.java
@@ -1,7 +1,13 @@
 package edu.miu.ecommerce.service;
 
+import edu.miu.ecommerce.domain.Invoice;
+import edu.miu.ecommerce.domain.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface InvoiceService {
+
+    Invoice generateInvoice(List<Order> orders);
 }
