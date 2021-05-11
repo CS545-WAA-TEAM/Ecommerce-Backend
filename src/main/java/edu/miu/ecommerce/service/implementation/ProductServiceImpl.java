1f64e61ec8545e9e53de3ef88d3b92d4609435ba
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:53:08 -0500

    product service

--- src/main/java/edu/miu/ecommerce/service/implementation/ProductServiceImpl.java
+++ src/main/java/edu/miu/ecommerce/service/implementation/ProductServiceImpl.java
@@ -1,6 +1,31 @@
 package edu.miu.ecommerce.service.implementation;
 
+import edu.miu.ecommerce.domain.Product;
 import edu.miu.ecommerce.service.ProductService;
 
 public class ProductServiceImpl implements ProductService {
+    @Override
+    public Iterable<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(long id) {
+        return null;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, long id) {
+
+    }
 }
