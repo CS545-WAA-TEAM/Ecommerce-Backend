dadd4397f1542938f03f27052621610eae46bf52
JaCkIsO <jackisodev@gmail.com>
2021-05-11 00:33:06 -0500

    Update PersistenceConfig.java
    Remove redundent annotations

--- src/main/java/edu/miu/ecommerce/configuration/PersistenceConfig.java
+++ src/main/java/edu/miu/ecommerce/configuration/PersistenceConfig.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories
 @EnableJpaAuditing
 public class PersistenceConfig {
 }
