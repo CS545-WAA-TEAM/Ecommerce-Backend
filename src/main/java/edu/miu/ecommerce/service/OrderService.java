cabcc1591ac8f766b962f983754be6d279b86f2b
beeAk <se.blen.aklilu@gmail.com>
2021-05-11 12:52:49 -0500

    Order service

--- src/main/java/edu/miu/ecommerce/service/OrderService.java
+++ src/main/java/edu/miu/ecommerce/service/OrderService.java
@@ -1,7 +1,24 @@
 package edu.miu.ecommerce.service;
 
+import edu.miu.ecommerce.domain.Order;
+import edu.miu.ecommerce.domain.Product;
+import edu.miu.ecommerce.domain.ShoppingCart;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface OrderService {
+
+    Iterable<Order> getAllOrders();
+
+    Order getOrderById(long id);
+
+    Order addOrder(Order order);
+
+    void deleteOrder(long id);
+
+    Product findProductByOrderId();
+
+
 }
--- src/main/java/edu/miu/ecommerce/service/implementation/OrderServiceImpl.java
+++ src/main/java/edu/miu/ecommerce/service/implementation/OrderServiceImpl.java
@@ -1,6 +1,32 @@
 package edu.miu.ecommerce.service.implementation;
 
+import edu.miu.ecommerce.domain.Order;
+import edu.miu.ecommerce.domain.Product;
 import edu.miu.ecommerce.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
+    @Override
+    public Iterable<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public Order getOrderById(long id) {
+        return null;
+    }
+
+    @Override
+    public Order addOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(long id) {
+
+    }
+
+    @Override
+    public Product findProductByOrderId() {
+        return null;
+    }
 }
