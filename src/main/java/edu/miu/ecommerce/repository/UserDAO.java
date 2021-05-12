9f82931b9467c0611abd8c5167834c5bb52926d2
JaCkIsO <jackisodev@gmail.com>
2021-05-12 11:02:04 -0500

    Update UserDAO.java
    Update UserDAO with findByUsername

--- src/main/java/edu/miu/ecommerce/repository/UserDAO.java
+++ src/main/java/edu/miu/ecommerce/repository/UserDAO.java
@@ -3,5 +3,10 @@ package edu.miu.ecommerce.repository;
 import edu.miu.ecommerce.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserDAO extends CrudRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
 }
